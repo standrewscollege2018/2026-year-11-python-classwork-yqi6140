4315afa67a482ad1db7ea524ae6e77d6a24917ea
yqi6140 <yqi6140@stacmail.net>
2026-02-05 12:05:18 +1300

    Create hellowworld.py

---
+++ hellowworld.py
@@ -0,0 +1,14 @@
+# this is print
+import random
+xs = 2
+abat =["a","b","c","d","e","f",g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z]
+print("hellow World")
+if (x == 2):
+    print (" hi this is a precorded message")
+def xs():
+    while True:
+        x=random.randint(1,24)
+        print(x)
+        if (x == 1):
+            break
+xs()
