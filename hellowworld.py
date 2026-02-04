21ef684c330f6809dc4c756fe3026be572fb05dc
yqi6140 <yqi6140@stacmail.net>
2026-02-05 12:15:15 +1300

    Update hellowworld.py

--- hellowworld.py
+++ hellowworld.py
@@ -1,14 +1,16 @@
-# this is print
+# this is a program
 import random
+wa = "n"
 xs = 2
-abat =["a","b","c","d","e","f",g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z]
+abat =["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"]
 print("hellow World")
-if (x == 2):
+if (xs == 2):
     print (" hi this is a precorded message")
 def xs():
     while True:
-        x=random.randint(1,24)
-        print(x)
-        if (x == 1):
+        xs=random.randint(1,25)
+        if (xs == 25):
             break
+        wa=abat(xs)
+        print(wa)     
 xs()
